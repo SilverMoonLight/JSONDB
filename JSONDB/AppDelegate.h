68e723375b2353acda2bdf2abb66e2b187c30490
John <Jon@johns-mac.local>
2015-11-30 08:46:01 -0500

    Initial Commit

---
+++ JSONDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSONDB
+//
+//  Created by John on 11/30/15.
+//  Copyright © 2015 Jona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSONDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSONDB
+//
+//  Created by John on 11/30/15.
+//  Copyright © 2015 Jona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
